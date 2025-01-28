54b92d6cc38538a01273aafdc67f0e77efcafc57
Axtheris <kevinmanso06@gmail.com>
2025-01-28 00:01:17 -0500

    test

---
+++ src/main/java/com/axther/autility/AutilityClient.java
@@ -0,0 +1,10 @@
+package com.axther.autility;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutilityClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/axther/autility/AutilityDataGenerator.java
@@ -0,0 +1,11 @@
+package com.axther.autility;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AutilityDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
